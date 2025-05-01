8362eaf2a5ca6534d18fbb41838fe7a2f150ed84
Ciprian Eniță <ciprian_enita@yahoo.com>
2025-05-01 14:45:26 +0300

    complete ERC3643 implementation

--- contracts/src/token/ERC3643.sol
+++ contracts/src/token/ERC3643.sol
@@ -1,23 +1,39 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.29;
 
-import {ERC20} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {
+    ERC20Burnable, ERC20
+} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "../../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {IIdentityRegistry} from "./interfaces/IIdentityRegistry.sol";
 import {ICompliance} from "./interfaces/ICompliance.sol";
 
-contract ERC3643Token is ERC20 {
+/// @title ERC3643Token
+/// @author AlinCiprian
+/// @notice This contract is an implementation of an ERC20 Token with several conditions on top of it.
+/// Whenever a collective buy campaign is succesful, a new ERC3643 contrac will be deployed, with a total
+/// of 100 tokens which will be minted to the campaign contract.
+
+contract ERC3643Token is ERC20, ERC20Burnable, Ownable {
     error ERC3643Token__SenderNotVerified();
     error ERC3643Token__RecipientNotVerified();
     error ERC3643Token__TransferNotCompliant();
+    error ERC3643Token__NotZeroAddress();
+    error ERC3643Token__MintAmountIsZero();
 
     IIdentityRegistry public identityRegistry;
     ICompliance public compliance;
 
+    // The total supply that will ever exist is 100 tokens, each one representing 1 % ownership of the entire asset
+    uint256 totalSupply = 100;
+
     constructor(string memory _name, string memory _symbol, address _identityRegistryAdress, address _complianceAddress)
         ERC20(_name, _symbol)
+        Ownable(msg.sender)
     {
         identityRegistry = IIdentityRegistry(_identityRegistryAdress);
         compliance = ICompliance(_complianceAddress);
+        _mint(msg.sender, totalSupply);
     }
 
     // Override function to check for compliance before executing
@@ -30,6 +46,13 @@ contract ERC3643Token is ERC20 {
     function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
         _checkCompliance(from, to, amount);
         return super.transferFrom(from, to, amount);
+    }
+
+    /// The entire amount of tokens is burnt.
+    /// @dev Meant to be used once the asset they represent is sold.
+    /// @dev It burns the entire supply at once
+    function burnTokens() external onlyOwner {
+        burn(totalSupply);
     }
 
     function _checkCompliance(address from, address to, uint256 amount) internal view {
--- contracts/src/token/compliance/Compliance.sol
+++ contracts/src/token/compliance/Compliance.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.29;
 import {ICompliance} from "../interfaces/ICompliance.sol";
 
 contract Compliance is ICompliance {
-    // Simple placeholder compliance check; you can add more complex checks.
+    // Add checks here
     function canTransfer(address from, address to, uint256 amount) external pure override returns (bool) {
         return true; // Placeholder, allow all transfers.
     }
