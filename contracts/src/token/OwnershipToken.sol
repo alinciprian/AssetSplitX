fa1db19913f505498ad5ee25a5024dee7599806d
Ciprian Eniță <ciprian_enita@yahoo.com>
2025-05-04 10:25:25 +0300

    ERC20Permit implementation

--- contracts/src/token/OwnershipToken.sol
+++ contracts/src/token/OwnershipToken.sol
@@ -1,67 +1,60 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import {
-    ERC20Burnable, ERC20
-} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Permit} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import {Ownable} from "../../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {IIdentityRegistry} from "./interfaces/IIdentityRegistry.sol";
 import {ICompliance} from "./interfaces/ICompliance.sol";
 
-/// @title OwnershipToken
-/// @author AlinCiprian
-/// @notice This contract is an implementation of an ERC20 Token with several conditions on top of it.
-/// Whenever a collective buy campaign is succesful, a new ERC3643 contract will be deployed, with a total
-/// of 100 tokens which will be minted to the campaign contract.
-
-contract OwnershipToken is ERC20, ERC20Burnable, Ownable, ERC20Votes {
+contract OwnershipToken is ERC20Permit, ERC20Votes, Ownable {
     error ERC3643Token__SenderNotVerified();
     error ERC3643Token__RecipientNotVerified();
     error ERC3643Token__TransferNotCompliant();
-    error ERC3643Token__NotZeroAddress();
-    error ERC3643Token__MintAmountIsZero();
 
     IIdentityRegistry public identityRegistry;
     ICompliance public compliance;
 
-    // The total supply that will ever exist is 100 tokens, each one representing 1 % ownership of the entire asset
-    uint256 totalSupply = 100 ether;
+    uint256 public constant MAX_SUPPLY = 100 ether;
 
-    constructor(string memory _name, string memory _symbol, address _identityRegistryAdress, address _complianceAddress)
-        ERC20(_name, _symbol)
-        Ownable(msg.sender)
-    {
-        identityRegistry = IIdentityRegistry(_identityRegistryAdress);
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _identityRegistryAddress,
+        address _complianceAddress
+    ) ERC20(_name, _symbol) ERC20Permit(_name) Ownable(msg.sender) {
+        identityRegistry = IIdentityRegistry(_identityRegistryAddress);
         compliance = ICompliance(_complianceAddress);
-        _mint(msg.sender, totalSupply);
+        _mint(msg.sender, MAX_SUPPLY);
     }
 
-    // Override function to check for compliance before executing
     function transfer(address to, uint256 amount) public override returns (bool) {
         _checkCompliance(_msgSender(), to, amount);
         return super.transfer(to, amount);
     }
 
-    // Override function to check for compliance before executing
     function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
         _checkCompliance(from, to, amount);
         return super.transferFrom(from, to, amount);
     }
 
-    /// The entire amount of tokens is burnt.
-    /// @dev Meant to be used once the asset they represent is sold.
-    /// @dev It burns the entire supply at once
     function burnTokens() external onlyOwner {
-        burn(totalSupply);
+        _burn(msg.sender, MAX_SUPPLY);
     }
 
     function _checkCompliance(address from, address to, uint256 amount) internal view {
-        // Ensure both sender and recipient are verified in the identity registry
         if (!identityRegistry.isVerified(from)) revert ERC3643Token__SenderNotVerified();
         if (!identityRegistry.isVerified(to)) revert ERC3643Token__RecipientNotVerified();
-
-        // Ensure the transfer complies with the rules
         if (!compliance.canTransfer(from, to, amount)) revert ERC3643Token__TransferNotCompliant();
+    }
+
+    function _update(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, amount);
+    }
+
+    function nonces(address owner) public view override(ERC20Permit) returns (uint256) {
+        return super.nonces(owner);
     }
 }
